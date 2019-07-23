12096ea03d1c789954021956e896ff11fa603ddf
imad-ramy <turlax800@outlook.fr>
2019-07-23 15:27:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default class App extends React.Component {
+  state ={
+    text:"", 
+    listoftext:[""] 
+   }
+
+   render(){
+
+return ( 
+   <div>
+        <input   value={this.state.text}
+        onChange={(e) =>{this.setState({text: e.target.value})}}
+        placehoder="ajoute nouveau"
+        />
+        <button onClick={() => {
+            const PreviousState=this.state.listoftext;  
+            const nextState=[this.state.text];  
+            this.setState({listoftext:[...PreviousState, ...nextState]}) 
+            this.setState({task:''})
+          }}>Add</button> 
+          { this.state.listoftext.map((e,i) =><div>
+              <li>{e}</li>           
+
+              <button onClick={() => {
+              let array = this.state.listoftext
+              let index = i
+              array.splice(i,1)
+              this.setState({listoftext:array})
+              }}>delete</button>
+          </div>)} 
+</div>
+   )}}
